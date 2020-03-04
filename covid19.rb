01eab556b7a78db117eefacee1b8cb4600bdce7b
Toshiaki Asai <toshi.alternative@gmail.com>
2020-03-04 22:33:33 +0900

    ちょっとは遊ばないと報われない

--- covid19.rb
+++ covid19.rb
@@ -11,10 +11,22 @@ Plugin.create(:covid19) do
   filter_extract_datasources do |dss|
     [{
        **dss,
-       covid19_contacts: '新型コロナコールセンター相談件数',
-       covid19_patients: '罹患者',
+       covid19_contacts:         '新型コロナコールセンター相談件数',
+       covid19_patients:         '罹患者',
        covid19_patients_summary: '陽性患者数',
      }]
+  end
+
+  def update(res)
+    case JSON.parse(res.body, symbolize_names: true)
+    in {
+      contacts:         {data: contacts},
+      patients:         {data: patients},
+      patients_summary: {data: patients_summaries} }
+      Plugin.call(:extract_receive_message, :covid19_contacts, contacts.map{|c| Plugin::Covid19::Contact.new(c) })
+      Plugin.call(:extract_receive_message, :covid19_patients, patients.map.with_index{|c, i| Plugin::Covid19::Patient.new({**c, number: i + 1}) })
+      Plugin.call(:extract_receive_message, :covid19_patients_summary, patients_summaries.map{|c| Plugin::Covid19::PatientsSummary.new(c) })
+    end
   end
 
   def polling
@@ -25,13 +37,7 @@ Plugin.create(:covid19) do
       res = http.request(Net::HTTP::Get.new(@url.path))
       case res
       when Net::HTTPSuccess     # 2xx
-        data = JSON.parse(res.body, symbolize_names: true)
-        contacts = data.dig(:contacts, :data)
-        Plugin.call(:extract_receive_message, :covid19_contacts, contacts.map{|c| Plugin::Covid19::Contact.new(c) })
-        patients = data.dig(:patients, :data)
-        Plugin.call(:extract_receive_message, :covid19_patients, patients.map.with_index{|c, i| Plugin::Covid19::Patient.new({**c, number: i + 1}) })
-        patients_summaries = data.dig(:patients_summary, :data)
-        Plugin.call(:extract_receive_message, :covid19_patients_summary, patients_summaries.map{|c| Plugin::Covid19::PatientsSummary.new(c) })
+        update(res)
       else
         post_apocalypse
       end
